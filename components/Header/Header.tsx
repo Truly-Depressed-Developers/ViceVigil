b1ebae0948147f2db1058f43cb749db92377a2c0
Spookyless <spookyless.info@gmail.com>
2024-01-21 10:30:26 +0100

    Changed title

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import {Avatar, CircularProgress} from "@mui/material";
+import localFont from 'next/font/local';
+
+const daysOne = localFont({
+  src: '../../app/DaysOne-Regular.ttf',
+  display: 'swap',
+});
 
 export const Header: React.FC = () => {
   return (
     <div className={styles["header"]}>
       <span className={styles['sync']}>
-        <CircularProgress size={12} className={styles["spinner"]}/>
-        Synchronizing...
+        Synchronized!
       </span>
 
-      <span className={styles['name']}>
-        NAME
+      <span className={[styles['name'], daysOne.className].join(" ")}>
+        ViceVigil
       </span>
 
       <span className={styles["user-container"]}>

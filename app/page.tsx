cb3f1493cda393003b95580321441d29b77ff090
Jakub Kwiecień <kkwiecie@interia.pl>
2024-01-21 01:56:57 +0100

    added daily streaks

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,48 @@
 import { QuoteBar } from '@/components/Main/QuoteBar/QuoteBar';
 import { Statistics } from '@/components/Main/Statistics/Statistics';
 import { StatusBar } from '@/components/Main/StatusBar/StatusBar';
+import { StreaksBar } from '@/components/Main/StreaksBar/StreaksBar';
+import { eachStreakType } from '@/components/Main/StreaksBar/StreaksBar';
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { useState } from 'react';
 
 const quoteData = [
   '"Sukces to suma małych wysiłków, powtarzanych dzień po dniu."',
   '"text2"'
+];
+
+
+let streaksDataDefault: eachStreakType[] = [
+  {
+    streak: 12,
+    type: "Not drinking alcohol"
+  },
+  {
+    streak: 10,
+    type: "Not smoking cigarettes"
+  },
+  {
+    streak: 8,
+    type: "Not listening to music"
+  },
+  {
+    streak: 3,
+    type: "Not drinking energizers"
+  },
+  {
+    streak: 3,
+    type: "Not taking drugs"
+  }
 ]
 
 const Page: React.FC = () => {
+  const [streaksData, setStreaksData] = useState<eachStreakType[]>(streaksDataDefault);
+  useEffect(() => {
+    streaksData.sort((a, b) => b.streak - a.streak);
+    console.log(streaksData);
+    setStreaksData(streaksData);
+  }, [streaksData])
   const [activeQuote, setActiveQuote] = useState(0);
   return (
     <Box sx={{
@@ -23,6 +55,7 @@ const Page: React.FC = () => {
     }}>
       <StatusBar></StatusBar>
       <QuoteBar quoteData={{ text: quoteData[activeQuote] }}></QuoteBar>
+      <StreaksBar streaksData={streaksData}></StreaksBar>
       <Statistics></Statistics>
 
     </Box>
